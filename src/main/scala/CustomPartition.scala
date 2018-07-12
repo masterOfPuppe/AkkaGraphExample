cfa3b92c44bf0b88df7c03c596475c51b7c4f0e9
masterOfPuppe <levojokis@larjem.com>
2018-07-12 10:36:57 +0200

    add new functionality using an external sink

--- src/main/scala/CustomPartition.scala
+++ src/main/scala/CustomPartition.scala
@@ -1,20 +1,25 @@
-import akka.NotUsed
-import akka.stream.scaladsl.{Flow, GraphDSL, Partition}
-import akka.stream.FanOutShape2
+import akka.{Done, NotUsed}
+import akka.stream.scaladsl.{Flow, GraphDSL, Partition, Sink}
+import akka.stream.{FanOutShape2, FlowShape, SinkShape}
+
+import scala.concurrent.Future
 
 object CustomPartition {
 
-  def apply[Int]() = GraphDSL.create[FanOutShape2[scala.Int, scala.Int, scala.Int]](){
+  /*Filter discards all flows in left sink. The sink is passed and define outside.
+  * The left side is passed throw. In the Partition is specify the function used to divide the flow and the number of branches.
+  * After it's necessary to specify the output destination of each branch.*/
+  def apply[String](sink : Sink[Any, Future[Done]]) = GraphDSL.create[FlowShape[String, scala.Predef.String]]() {
     implicit builder: GraphDSL.Builder[NotUsed] =>
-    import GraphDSL.Implicits._
+      import GraphDSL.Implicits._
 
-    val left = builder.add(Flow[scala.Int])
-    val right = builder.add(Flow[scala.Int])
-    val partition = builder.add(Partition[scala.Int](2, x => if(x < 5) 0 else 1))
+      val partition = builder.add(Partition[String](2, x => if(x.toString.length < 5) 0 else 1))
+      val left = builder.add(Flow[String].map(x => s"DONE ${x}"))
 
-    partition.out(0) ~> left.in
-    partition.out(1) ~> right.in
+      partition.out(0) ~> sink
+      partition.out(1) ~> left.in
 
-    new FanOutShape2(partition.in, left.out, right.out)
+      // This is the output passed where is specify who is the input and the unique output (we are using FlowShape and not FanOutShape#)
+      new FlowShape(partition.in, left.out)
   }
 }
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,24 +1,38 @@
 import akka.NotUsed
 import akka.actor.ActorSystem
-import akka.stream.scaladsl.{Flow, GraphDSL, Partition, RunnableGraph, Sink, Source}
+import akka.stream.scaladsl.{Flow, Framing, GraphDSL, Partition, RunnableGraph, Sink, Source, StreamConverters}
 import akka.stream.{ActorMaterializer, ClosedShape}
+import akka.util.ByteString
 import cats.data._
 
 object Main extends App {
   implicit val actorSystem = ActorSystem("validationStageDemo")
   implicit val materializer = ActorMaterializer()
 
+  /*Create a graph that read a file content like a stream,
+  discards lines with length more then 5, while prints the others lines*/
   RunnableGraph.fromGraph(GraphDSL.create() { implicit builder: GraphDSL.Builder[NotUsed] =>
     import GraphDSL.Implicits._
-    val in = Source(1 to 10)
+
+    // read content file like a stream, parsing with frame length of 1024*1024
+    val data = this.getClass.getResourceAsStream("/file_to_process.txt")
+    val in = StreamConverters.fromInputStream(() => data)
+      .via(Framing.delimiter(ByteString("\n"), 1024 * 1024, true))
+      .map(_.utf8String)
+
+    // create a sink for good lines and print them
     val out = Sink.foreach(println)
-    val discardSink = Sink.ignore
 
-    val partVal = builder.add(CustomPartition[Int])
+    // create a sink to discard bad lines (length less of 5)
+    val sinkDiscard = Sink.foreach[Any](x => println(s"DISCARD FLOW ${x}"))
 
-    in ~> partVal.in
-    partVal.out0 ~> out
-    partVal.out1 ~> discardSink
+    /* create a new component like a graph and add this to the courrent graph using the functionality of .add.
+    The use of .add is necessary when it's necessary add another graph to the current one
+    or when you want specify the input and the output of your component*/
+    val partVal = builder.add(CustomPartition[String](sinkDiscard))
+
+    // Define the flow for current graph
+    in ~> partVal ~> out
 
     ClosedShape
   }).run()
